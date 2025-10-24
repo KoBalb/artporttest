7ead63c6675f8dacaeaa900b9f7921ee8c1ed5dd
KoBalb <artem.kovalyk79@gmail.com>
2025-10-24 20:15:27 +0300

    feat 7 : add sort

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,47 @@
 'use client'
 import Card from "@/components/VagonsCard/Card";
 import { useVagons } from "@/hooks/vagonHooks";
-import { h1 } from "framer-motion/client";
+import { Button, HStack, VStack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [wagons, setWagons] = useState<Vagon[]>([]);
+
   const {data, isLoading} = useVagons();
-  if  (isLoading){
-    return <h1>Loading</h1>
+  
+  useEffect(() => {
+  if (!isLoading) {
+    setWagons(data);
   }
+  }, [data]);
+
+  const sortByNumber = () => {
+    const sorted = [...wagons].sort((a, b) => a.VagonNumber - b.VagonNumber);
+    setWagons(sorted);
+  };
+
+  const sortByStation = () => {
+    const sorted = [...wagons].sort((a, b) => a.DepartureStationName.localeCompare(b.DepartureStationName)
+  )
+    setWagons(sorted);
+  };
+
+
+  
   return (
     <>
+    <VStack spacing="4" align="start" padding="4">
+      <HStack spacing="4">
+        <Button onClick={sortByNumber} colorScheme="blue">Сортировать по номеру</Button>
+        <Button onClick={sortByStation} colorScheme="green">Сортировать по станции</Button>
+      </HStack>
       {
-        data.map(vagon => <Card
+        wagons.map(vagon => <Card
         key={vagon.VagonNumber}
         {...vagon}
         />)
       }
+    </VStack>
     </>
   );
 }
