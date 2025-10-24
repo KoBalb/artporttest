161ed2f475552387cfd093272e100b486eb6762f
KoBalb <artem.kovalyk79@gmail.com>
2025-10-24 23:30:35 +0300

    feature 9 : add infinite scroll

--- hooks/vagonHooks.ts
+++ hooks/vagonHooks.ts
@@ -1,19 +1,10 @@
 'use client'
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { Query, useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { getServerSideProps } from "../api/vagonApi";
 
 
-  export const useVagons = () =>
-    useQuery<Vagon[], Error>({
-        queryKey: ["vagons"],
-        queryFn: () => getServerSideProps(),
-    });
-
-
-    export const UseVagonsQuerry = () => 
-      useInfiniteQuery({
-        queryKey: ['vagons'],
-        queryFn: getServerSideProps(),
-        getNextPageParam: (lastPage, pages) => lastPage.nextCursor})
-
-
+export const useVagons = () =>
+  useQuery<Vagon[], Error>({
+      queryKey: ["vagons"],
+      queryFn: () => getServerSideProps(),
+  });
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,26 @@
 import Card from "@/components/VagonsCard/Card";
 import { useDebounce } from "@/hooks/useDebounceHook";
 import { useVagons } from "@/hooks/vagonHooks";
-import { Button, HStack, Input, VStack } from "@chakra-ui/react";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { Button, Flex, HStack, Input, SimpleGrid, VStack } from "@chakra-ui/react";
 import { useState } from "react";
 
 export default function Home() {
   const { data: wagons, isLoading } = useVagons();
 
+
   const [sortType, setSortType] = useState<SortType>('number');
   const [seacrhTerm, setSeacrhTerm] = useState('')
   const debouncedSearch = useDebounce(seacrhTerm)
 
+
+  const [visibleCount, setVisibleCount] = useState(1);
+
+  
+
+
   if (isLoading) return <h1>Loading...</h1>;
   if (!wagons) return <h1>No data</h1>;
-
+  
 
   const sortedWagons = [...wagons].sort((a, b) => {
     if (sortType === 'number') {
@@ -25,25 +31,48 @@ export default function Home() {
     }
   });
 
+  const filteredWagons = sortedWagons.filter(wagon =>
+  wagon.VagonNumber.toString().includes(debouncedSearch)
+  );
 
-  return (
-    <VStack spacing="4" align="start" padding="4">
-      <HStack spacing="4">
-        <Button onClick={() => setSortType('number')} colorScheme="blue">
+  const visibleWagons = filteredWagons.slice(0, visibleCount * 5);
+
+  return (<>
+    <VStack spacing="4" align="stretch" padding="4" w="100%">
+      <HStack spacing="4" w="100%">
+        <Button onClick={() => setSortType('number')} colorScheme="blue" flex="1">
           Сортировать по номеру
         </Button>
-        <Button onClick={() => setSortType('station')} colorScheme="green">
+        <Button onClick={() => setSortType('station')} colorScheme="green" flex="1">
           Сортировать по станции
         </Button>
-        <Input type="search"
-        placeholder="Поиск"
-        onChange={e => {setSeacrhTerm(e.target.value)}}
-        ></Input>
+        <Input
+          type="search"
+          placeholder="Поиск"
+          onChange={e => setSeacrhTerm(e.target.value)}
+          flex="2"
+        />
+        
       </HStack>
 
-      {sortedWagons.filter(wagon => wagon.VagonNumber.toString().includes(debouncedSearch)).map(vagon => (
-        <Card key={vagon.VagonNumber} {...vagon} />
-      ))}
+      <SimpleGrid columns={5} spacing="4">
+          {visibleWagons.map(vagon => (
+            <Card key={vagon.VagonNumber} {...vagon} />
+          ))}
+      </SimpleGrid>
+
+
+<HStack>
+  <Flex w="100%" justify="center">
+    <Button onClick={() => setVisibleCount(c => c + 1)}>
+      Показать ещё
+    </Button>
+  </Flex>
+
+  </HStack>
     </VStack>
-  );
+
+
+    
+  </>);
 }
