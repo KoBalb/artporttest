2fd682b09888437948ec0927ae6a021cdb779696
KoBalb <artem.kovalyk79@gmail.com>
2025-10-24 18:37:28 +0300

    feature(build, hooks) 2-3 : refactor and add hooks for Vagon

--- api/vagonApi.ts
+++ api/vagonApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export async function getVagons(): Promise<Vagon[]> {
+export async function getServerSideProps(): Promise<Vagon[]> {
   const res = await axios.get("https://rwl.artport.pro/commercialAgent/hs/CarrWorkApp/VagonInfo");
   return res.data;
 }
